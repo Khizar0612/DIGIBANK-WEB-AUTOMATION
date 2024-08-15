e2ef57bdc7272914b5630119d58588b43a8a6eca
Khizar Saeed <iamkhizarsaeed@gmail.com>
2024-08-15 14:53:29 +0500

    Updated

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,8 @@
+describe('Logout', ()=>{
+    it('Logout Functionality', ()=>{
+        cy.get("input[placeholder='Enter Your Password']").type('aassdd12345')
+        cy.get("").click()
+
+    })
+
+})
